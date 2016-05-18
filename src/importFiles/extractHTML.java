5ce0906317370ab230adbc68fd250d92afa598e6
Paul Muschiol <paul.muschiol@yahoo.de>
2016-05-18 11:22:54 +0200

    test

---
+++ src/importFiles/extractHTML.java
@@ -0,0 +1,167 @@
+package importFiles;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.Arrays;
+import java.util.List;
+
+public class extractHTML {
+
+	/**
+	 * input file
+	 * */
+	String trecLocation = "html/0.trecweb";
+	
+	/**
+	 * output location for separated files in folders named by there uri
+	 * */
+	String outLocation = "html/";
+	
+	/**
+	 * a <code>ArrayList</code> of my documents
+	 * */
+	List<String> myDocs = Arrays.asList(
+
+		"sanosil-service.de",
+		"santex-gmbh.de",
+		"scheidle.eu",
+		"scheufelen.com",
+		"schiller-gruppe.de",
+		"sohard.de",
+		"starke-rechtsanwaelte.de",
+		"steinrueck.de",
+		"tanquid.com",
+		"tecklenborg-kegel.de",
+		"tippertie.com",
+		"vacu-form.de",
+		"varial.de",
+		"venti-oelde.de",
+		"welser.com",
+		"wichmann.de",
+		"wilhelm-meier-online.de",
+		"wisap.de",
+		"wisy.de",
+		"zwt-engineering.com"
+			
+			);
+	
+	public extractHTML() {}
+	
+	public void read() {
+			
+			BufferedReader reader = null;
+			int docCtr = 0;
+			try {
+				reader = new BufferedReader( new InputStreamReader( new FileInputStream( new File(  this.trecLocation ) ) ) );
+				
+				String line = "";
+				String docTxt = "";
+				String docTxtOut = "";
+				boolean test = false;
+				
+				while( ( line = reader.readLine() ) != null ) {
+					
+					
+					//add lines to output
+					if( !line.isEmpty() ) {
+							docTxt += "\n" + line;
+							
+							//start of html doc
+							if(!test && line.contains("<html")){
+								test = true;
+							}
+							
+							//end of html doc
+							if(line.contains("</html>")){
+								test = false;
+								docTxtOut += "\n" + line;
+							}
+							
+							//add line to html output
+							if(test){
+								docTxtOut += "\n" + line;
+							}
+					}
+					
+					//end of Trecweb element -> output file
+					if( line.contains( "</DOC>" ) ) {
+						
+						docTxt += line;
+						String dir = "";
+						
+						//get domain name from trecweb header
+						if( ( dir = analyseHdr( docTxt ) ) != null ) {
+							
+							Writer writer = null;
+							
+							try{
+								File f = new File(this.outLocation + dir ); 
+								//create domain dir
+								if( !( f.isDirectory() ) ) {
+									f.mkdir();
+								}
+								//create output file
+								writer = new BufferedWriter( new OutputStreamWriter( new FileOutputStream( new File( 
+										( this.outLocation + dir + "/" + docCtr + ".html" ) ) ) ) );
+								docCtr++;
+								writer.write( docTxtOut );
+								
+								
+							} catch( IOException ioe ) { 
+								ioe.printStackTrace(); 
+							} finally{
+								try{
+									writer.close();
+								} catch( IOException ioe ) { ioe.printStackTrace();}
+							}
+						}
+						
+						
+						//reset outputs
+						docTxt = "";
+						docTxtOut = "";
+						test = false;
+					}
+				}
+				
+				
+				
+			} catch( IOException ioe ) {
+				ioe.printStackTrace();
+			} finally {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+	}
+	
+	private String analyseHdr( String _docTxt ) {
+
+		String hdr = _docTxt.substring( _docTxt.indexOf("<DOCHDR>"), _docTxt.indexOf( "</DOCHDR>" ) );
+
+		
+		//return url from URL List if the hdr contains it
+		for( String url : this.myDocs ) {
+			if( hdr.contains( url ) ) {
+				return url;
+			}
+		}
+		
+		//document is not in List -> dont output
+		return null;
+	}
+	
+	public static void main( String[] _args ) {
+		new extractHTML().read();
+	} 
+}
